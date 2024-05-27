d9ef09e4967ede39d78b589491110eb5252ea7c8
Łukasz Karwacki <lkarwacki3@interia.pl>
2024-05-27 21:38:01 +0200

    end of project, only potential test remained

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.naive_bayes import GaussianNB
 
 
-#Główna funkcja analizująca każde zdjęcie
+#Główna funkcja analizująca każde zdjęcie:
 def extract_features(image):
     image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     lower_hsv = np.array([0, 48, 80], dtype='uint8')
@@ -46,13 +46,13 @@ def extract_features(image):
             if angle <= np.pi / 2:
                 fingers_count += 1
 
-    if(fingers_count > 0):
+    if fingers_count > 0:
         fingers_count += 1
 
-    algorithm_answers.append(fingers_count)
     return cv2.contourArea(hand_contours), cv2.arcLength(hand_contours, True), fingers_count
 
 
+#Dwie funkcje służące do sortowania nazw plików alfabetycznie:
 def atoi(character):
     return int(character) if character.isdigit() else character
 
@@ -61,22 +61,22 @@ def classic_sort(text):
     return [atoi(character) for character in re.split(r'(\d+)', text)]
 
 
+#Posortowanie nazw zdjęć z folderu alfabetycznie:
 filenames = [filename for filename in os.listdir('Images') if filename.endswith('.png')]
 filenames.sort(key=classic_sort)
 
+#Analiza wszystkich zdjęć przez algorytm:
 extracted_features = []
-algorithm_answers = []
-iterator = 1
 for filename in filenames:
     if filename.endswith('.png'):
         image = cv2.imread(f'Images\\{filename}')
         if image is None:
             print(f'Failed opening image {filename}')
-        print(f'Analyzing hand{iterator}.png')
-        features = extract_features(image)
-        extracted_features.append(features)
-        iterator+=1
+        print(f'Analyzing {filename}')
+        features_from_single_image = extract_features(image)
+        extracted_features.append(features_from_single_image)
 
+#Wczytanie zbioru etykiet (poprawnych odpowiedzi dla każdego zdjęcia):
 labels = []
 with open('labels.txt', 'r', encoding='UTF-8') as file:
     for line in file:
@@ -89,8 +89,10 @@ with open('labels.txt', 'r', encoding='UTF-8') as file:
 
 
 #Przygotowanie danych dla klasyfikatorów:
+#"Konwersja" na data frame:
 extracted_features_data_frame = pd.DataFrame(extracted_features, columns=['Feature1', 'Feature2', 'Feature3'])
 
+#Normalizacja:
 scaler = MinMaxScaler()
 scaler.fit(extracted_features_data_frame)
 normalized_extracted_features_data_frame = pd.DataFrame(scaler.transform(extracted_features_data_frame),columns=extracted_features_data_frame.columns)
@@ -98,23 +100,23 @@ normalized_extracted_features_data_frame = pd.DataFrame(scaler.transform(extract
 #Podział na zbiory: testowy/treningowy w proporcji: 70% - trening, 30% - test
 X_train, X_test, y_train, y_test = train_test_split(normalized_extracted_features_data_frame, labels, test_size=0.3)
 
-#Użycie klasyfikatorów:
-#KNN:
+#Użycie klasyfikatorów (porównanie):
+print('\n------------------------\nWyniki klasyfikatorów\n------------------------')
+#KNN (dla trzech sąsiadów):
 knn_classifier = KNeighborsClassifier(3)
 knn_classifier.fit(X_train, y_train)
 print(f'KNN accuracy: {knn_classifier.score(X_test, y_test) * 100}%')
 
 #GaussianNB:
 gaussian_nb_classifier = GaussianNB()
-gaussian_nb_classifier.fit(X_train, y_train) #na zbiorze treningowym
+gaussian_nb_classifier.fit(X_train, y_train)
 print(f'GaussianNB accuracy: {gaussian_nb_classifier.score(X_test, y_test) * 100}%')
 
 
+#Do testu jakosci alogorytmu (tak na szybko):
 # sum = 0
-# for i in range(0, 1360):
+# for i in range(0, 1522):
 #     # print(f'{algorithm_answers[i]} == {labels[i]}?')
 #     if int(algorithm_answers[i]) == int(labels[i]):
 #         sum += 1
-# print(f'Wynik algorytmu: {sum}/1360')
-
-
+# print(f'Algorithm score: {sum}/1522')
